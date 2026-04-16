d07126bbbbb35e782c94c9b6bbcf75f77f431dfb
mdyeamin <ahmedyeamin05@gmail.com>
2026-04-16 15:53:03 +0600

    timeline functionality update up to date

--- src/app/states/page.jsx
+++ src/app/states/page.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+"use client";
+import { useContext } from "react";
+import { FriendsContext } from "../contexts/FriendsProvider";
 
 const States = () => {
-    return (
-        <div>
-            <h2 className="text-3xl">States</h2>
-        </div>
-    );
+  const { filterType } = useContext(FriendsContext);
+
+  const call = filterType.filter((call) => call.status === "call");
+  const text = filterType.filter((call) => call.status === "text");
+  const video = filterType.filter((call) => call.status === "video");
+//   console.log(call.length, "call data");
+  // console.log(text, "text data");
+  // console.log(video, "video data");
+
+  const cartData = {
+    call: call.length,
+    text: text.length,
+    video: video.length,
+  };
+  console.log(cartData);
+
+  return (
+    <div>
+      <h2 className="text-3xl">States</h2>
+    </div>
+  );
 };
 
 export default States;
